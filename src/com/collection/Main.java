303041e0860f03dd5cc2d588d7e59bd240c105b7
VitalinaShershan <vshershan@gmail.com>
2021-03-06 10:54:21 +0300

    Sun added but with errors

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -1,14 +1,20 @@
 package com.collection;
 
+import sun.security.provider.Sun;
+
 public class Main {
 
     public static void main(String[] args) {
 
-SolarSystem FactorX = new SolarSystem();
-FactorX.Sun=1;
-FactorX.Moon=3;
-FactorX.Planets=2;
+        SolarSystem FactorX = new SolarSystem();
+        FactorX.Sun = 1;
+        FactorX.Moon = 3;
+        FactorX.Planets = 2;
 
 
+        SUN Freon = new SUN();
+        Freon.color="500kj";
+        Freon.size="700km";
+        Freon.TEmp="40000c";
     }
 }
---
+++ src/com/collection/SUN.java
@@ -0,0 +1,7 @@
+package com.collection;
+
+public class SUN {
+    String size;
+    int TEmp;
+    String color;
+}
